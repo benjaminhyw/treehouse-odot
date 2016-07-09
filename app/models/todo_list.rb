6345f6ce4534b9304d76f3d72c519ff31c3c954a
Ben Flores <benjaminhyw@gmail.com>
2016-07-09 15:13:29 -0700

    Refactor todo list creation in spec

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,6 +1,7 @@
 class TodoList < ApplicationRecord
   validates_presence_of :title
-
   validates :title, length: {minimum: 3}
+  validates_presence_of :description
+  validates :description, length: {minimum: 5}
 
 end
